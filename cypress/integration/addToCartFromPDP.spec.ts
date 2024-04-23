2da358361444489c0a052d346d723f92567670fb
Daniel Velasco <danilovelga4747@gmail.com>
2024-04-23 12:45:33 -0500

    refactor: add skus clicking logic

--- cypress/integration/addToCartFromPDP.spec.ts
+++ cypress/integration/addToCartFromPDP.spec.ts
@@ -5,7 +5,7 @@ describe("Add to cart from PDP", () => {
     cy.setVtexIdCookie();
   });
 
-  it("Select a product summary and click to go to the PDP and add the product to the cart", () => {
+  it("Click on a product to go to the PDP and then click on the skus to select and finally add to cart", () => {
     cy.visit("/");
     cy.wait(3000);
     cy.fixture("addToCartFromPDP").then((contentElement) => {
@@ -14,6 +14,18 @@ describe("Add to cart from PDP", () => {
           content.eq(Math.floor(Math.random() * content.length))
         )
         .click();
+
+      cy.wait(2000);
+
+      cy.get(contentElement.colorSkuOption).then((content) =>
+        cy.chooseRandomSku(content)
+      );
+      cy.wait(1000);
+
+      cy.get(contentElement.sizeSkuOption).then((content) =>
+        cy.chooseRandomSku(content)
+      );
+
       cy.wait(3000);
       cy.get(contentElement.addToCartButton).click();
 
